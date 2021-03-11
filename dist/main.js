bccd993e7bb7c164737ccff8d2d3d61e61578e1c
MalladiChaitanya <chaitanya.kmc@gmail.com>
2021-03-11 22:29:23 +0530

    Base setup

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Plugin/Preset files are not allowed to export objects, only functions. In D:\\\\Projects\\\\donatethings\\\\node_modules\\\\babel-preset-react\\\\lib\\\\index.js\\n    at createDescriptor (D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:178:11)\\n    at D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:50\\n    at Array.map (<anonymous>)\\n    at createDescriptors (D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:29)\\n    at createPresetDescriptors (D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:101:10)\\n    at presets (D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:47:19)\\n    at mergeChainOpts (D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:315:26)\\n    at D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:278:7\\n    at buildRootChain (D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-chain.js:118:22)\\n    at loadPrivatePartialConfig (D:\\\\Projects\\\\donatethings\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\partial.js:57:55)\");\n\n//# sourceURL=webpack://donatethings/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
